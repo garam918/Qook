60af4045e94b7e8b740b2ea9c2adebc04691936f
imgaram <ijy918@naver.com>
2026-01-24 15:01:04 +0900

    feat : 현재 플랫폼 이름 수정

--- composeApp/src/iosMain/kotlin/com/garam/qook/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/com/garam/qook/Platform.ios.kt
@@ -3,7 +3,7 @@ package com.garam.qook
 import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+    override val name: String = UIDevice.currentDevice.systemName()
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
