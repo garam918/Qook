a0da69702cb58acd6b4d5ed41f335b49e7ff32c0
imgaram <ijy918@naver.com>
2026-01-24 15:03:33 +0900

    feat : iOS 메인 진입 시 Firebase, Koin 초기화

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,41 @@
 import SwiftUI
+import ComposeApp
+import FirebaseCore
+import FirebaseAuth
 
 @main
 struct iOSApp: App {
+
+    init() {
+
+        FirebaseApp.configure()
+        KoinKt.doInitKoinIos()
+
+        checkAndForceLogoutOnFirstRun()
+
+        // configureRevenueCat()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+
+    private func checkAndForceLogoutOnFirstRun() {
+        let hasRunBeforeKey = "hasRunBefore"
+        let userDefaults = UserDefaults.standard
+
+        if !userDefaults.bool(forKey: hasRunBeforeKey) {
+            do {
+
+                try Auth.auth().signOut()
+                print("First run detected: Forced Firebase sign out to clear keychain.")
+            } catch {
+                print("Error signing out on first run: \(error)")
+            }
+
+            userDefaults.set(true, forKey: hasRunBeforeKey)
         }
     }
 }
