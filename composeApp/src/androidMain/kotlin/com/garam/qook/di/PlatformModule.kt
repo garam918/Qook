d10d834d79ff24768d9fff094324ca9e4e26ee6d
imgaram <ijy918@naver.com>
2026-01-24 14:59:55 +0900

    feat : 각 플랫폼 별 모듈 구현

---
+++ composeApp/src/androidMain/kotlin/com/garam/qook/di/PlatformModule.kt
@@ -0,0 +1,15 @@
+package com.garam.qook.di
+
+import com.garam.qook.com.garam.qook.data.getDatabaseBuilder
+import com.garam.qook.data.QookDatabase
+import com.garam.qook.data.getTodoDatabase
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+
+actual fun platformModule(): Module = module {
+    single<QookDatabase> {
+        val builder = getDatabaseBuilder(context = get())
+        getTodoDatabase(builder)
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/garam/qook/di/PlatformModule.kt
@@ -0,0 +1,5 @@
+package com.garam.qook.di
+
+import org.koin.core.module.Module
+
+expect fun platformModule(): Module
