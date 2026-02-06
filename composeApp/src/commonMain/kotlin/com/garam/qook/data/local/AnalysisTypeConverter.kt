d823e466f06f5df8a93ab80657dd68bb8a2f94c0
imgaram <ijy918@naver.com>
2026-02-07 00:53:28 +0900

    feat : Analysis Data를 위한 Type Converter 구현

---
+++ composeApp/src/commonMain/kotlin/com/garam/qook/data/local/AnalysisTypeConverter.kt
@@ -0,0 +1,28 @@
+package com.garam.qook.data.local
+
+import androidx.room.TypeConverter
+import com.garam.qook.data.IngredientCategory
+import com.garam.qook.data.IngredientItem
+import kotlinx.serialization.json.Json
+
+class AnalysisTypeConverter {
+
+    @TypeConverter
+    fun fromCategoryString(value: String) : List<IngredientCategory> {
+        return Json.decodeFromString(value)
+    }
+
+    @TypeConverter
+    fun fromCategoryMap(list: List<IngredientCategory>) : String {
+        return Json.encodeToString(list)
+    }
+    @TypeConverter
+    fun fromString(value: String) : List<IngredientItem> {
+        return Json.decodeFromString(value)
+    }
+
+    @TypeConverter
+    fun fromMap(list: List<IngredientItem>) : String {
+        return Json.encodeToString(list)
+    }
+}
