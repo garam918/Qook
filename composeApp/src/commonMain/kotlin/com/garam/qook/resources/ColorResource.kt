505e8de79afafad70ef53f83273204e5e986a277
imgaram <ijy918@naver.com>
2026-02-10 23:29:56 +0900

    feat : Color Resource 수정

--- composeApp/src/commonMain/kotlin/com/garam/qook/resources/ColorResource.kt
+++ composeApp/src/commonMain/kotlin/com/garam/qook/resources/ColorResource.kt
@@ -3,7 +3,7 @@ package com.garam.qook.resources
 import androidx.compose.ui.graphics.Color
 
 
-val mainColor = Color(0xFF0DF20D)
+val mainColor = Color(0xFF6EF76E)
 val mainBackgroundColor = Color(0xFFF5F8F5)
 
 val subColor = Color(0xFF000000)
@@ -13,3 +13,6 @@ val cardBorderColor = Color(0xFFF3F4F6)
 val linkTextFieldBorderColor = Color(0xFFC9E6C9)
 
 val googleLoginBtnBorderColor = Color(0xFFE2E2E5)
+
+val lightGreen = Color(0xFFF0FFF0)
+val brandGreen = Color(0xFF00E600)
