cc81779a11eb3d961d00cf9147e3e01395682ca7
imgaram <ijy918@naver.com>
2026-02-10 23:29:07 +0900

    feat : MainViewModel 정의

--- composeApp/src/commonMain/kotlin/com/garam/qook/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/garam/qook/MainViewModel.kt
@@ -1,14 +1,32 @@
 package com.garam.qook
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.garam.qook.auth.AuthRepository
+import com.garam.qook.data.local.LocalUserData
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class MainViewModel(private val authRepository: AuthRepository) : ViewModel() {
 
+    private val _currentUser = MutableStateFlow<LocalUserData?>(null)
+    val currentUser = _currentUser.asStateFlow()
 
-    val currentUser = authRepository.currentUser()
+    init {
+
+        viewModelScope.launch {
+            _currentUser.value = authRepository.currentUser()
+
+        }
+
+    }
+
+    suspend fun updateUser(user: LocalUserData) = authRepository.updateUserInfo(user)
 
 
+
+    fun getLoggedIn() = authRepository.isLoggedIn()
 
 
 }
