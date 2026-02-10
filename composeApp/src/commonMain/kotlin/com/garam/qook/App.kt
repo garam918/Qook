bf7daa759f6c69337e37b9f6715c3ff82201726b
imgaram <ijy918@naver.com>
2026-02-10 23:31:37 +0900

    feat : 앱 메인 진입 화면 구현

--- composeApp/src/commonMain/kotlin/com/garam/qook/App.kt
+++ composeApp/src/commonMain/kotlin/com/garam/qook/App.kt
@@ -1,49 +1,118 @@
 package com.garam.qook
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
-
-import qook.composeapp.generated.resources.Res
-import qook.composeapp.generated.resources.compose_multiplatform
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.ui.NavDisplay
+import com.garam.qook.revenueCatTest.RevenueCatRepository
+import com.garam.qook.ui.groceryList.GroceryListScreen
+import com.garam.qook.ui.home.HomeScreen
+import com.garam.qook.ui.navigation.Route
+import com.garam.qook.ui.onboarding.OnBoardingScreen
+import com.garam.qook.ui.paywall.PaywallScreen
+import com.garam.qook.ui.result.AnalysisScreen
+import kotlinx.coroutines.launch
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 @Preview
-fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+fun App(mainViewModel: MainViewModel = koinViewModel()) {
+
+    val currentUser = mainViewModel.currentUser.collectAsState()
+
+    val isLoggedIn = mainViewModel.getLoggedIn()
+
+    val revenueCatRepo = remember { RevenueCatRepository() }
+
+    val updateUserScope = rememberCoroutineScope()
+
+    var appSessionId by remember { mutableStateOf(0) }
+
+    LaunchedEffect(currentUser.value) {
+
+        val isActiveSubscription = revenueCatRepo.hasActiveEntitlement()
+
+        println("isActiveSubscription $isActiveSubscription")
+
+        println("currentUser ${currentUser.value}")
+
+        currentUser.value?.let { user ->
+            updateUserScope.launch {
+                mainViewModel.updateUser(user.copy(paid = isActiveSubscription))
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        }
+    }
+
+    key(appSessionId) {
+        MaterialTheme {
+            val backStack =
+                remember { mutableStateListOf(if (isLoggedIn) Route.RouteHome() else Route.RouteOnBoarding) }
+//            remember { mutableStateListOf(if (true) Route.RouteHome() else Route.RouteOnBoarding) }
+            NavDisplay(
+                backStack = backStack,
+                onBack = { if (backStack.size > 1) backStack.removeLastOrNull() },
+                entryProvider = entryProvider {
+
+                    entry<Route.RouteOnBoarding> {
+                        OnBoardingScreen(onNavigateToHome = {
+                            backStack.clear()
+                            backStack.add(it)
+                        })
+                    }
+
+                    entry<Route.RouteHome> {
+                        HomeScreen(onNavigationToResult = {
+                            backStack.add(Route.RouteResult(data = it))
+                        }, onNavigationToPaywall = {
+                            backStack.add(Route.RoutePaywall)
+                        }, onNavigationToOnboarding = {
+                            backStack.clear()
+                            backStack.add(Route.RouteOnBoarding)
+                        }, onNavigationToGrocery = {
+
+                            backStack.add(Route.RouteGrocery)
+
+                        })
+                    }
+
+                    entry<Route.RouteResult> { key ->
+
+                        AnalysisScreen(onBackBtn = {
+                            backStack.remove(key)
+                        }, data = key.data, isPaid = currentUser.value?.paid ?: false)
+                    }
+
+                    entry<Route.RoutePaywall> { key ->
+                        PaywallScreen(
+                            revenueCatRepo,
+                            onDismiss = {
+                                backStack.remove(key)
+                            },
+                            onPurchaseComplete = { currentUser ->
+
+                                updateUserScope.launch {
+                                    mainViewModel.updateUser(currentUser.copy(paid = true))
+                                }.invokeOnCompletion {
+                                    backStack.clear()
+                                    backStack.add(Route.RouteHome())
+                                    appSessionId++
+                                }
+                            },
+                            currentUser = currentUser.value ?: return@entry,
+                        )
+                    }
+
+                    entry<Route.RouteGrocery> {
+                        GroceryListScreen(
+                            onBackBtn = {
+                                backStack.remove(it)
+                            }
+                        )
+                    }
+                },
+
+                )
         }
     }
 }
