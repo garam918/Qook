9e2332394da3999e4c86861455603fa0d7b8b77f
imgaram <ijy918@naver.com>
2026-02-10 23:29:47 +0900

    feat : User Data class 수정

--- composeApp/src/commonMain/kotlin/com/garam/qook/data/local/LocalUserData.kt
+++ composeApp/src/commonMain/kotlin/com/garam/qook/data/local/LocalUserData.kt
@@ -1,9 +1,20 @@
 package com.garam.qook.data.local
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
+import kotlin.uuid.ExperimentalUuidApi
 
+@OptIn(ExperimentalUuidApi::class)
+@Serializable
+@Entity(
+    tableName = "user"
+)
 data class LocalUserData(
-    val uid : String,
-    val email: String?,
-    val loginType : String
+    @PrimaryKey val uid : String = "",
+    val email: String? = "",
+    val loginType : String = "",
+    val paid : Boolean = false,
+    val usageCount: Int = 0,
+    val lastUseDate : String = ""
 )
