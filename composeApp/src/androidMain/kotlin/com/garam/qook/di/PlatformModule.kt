bc1e5e4bd3da41e579eed9b4831fa3b542108efa
imgaram <ijy918@naver.com>
2026-02-10 23:28:52 +0900

    feat : Database 패키지 수정

--- composeApp/src/androidMain/kotlin/com/garam/qook/di/PlatformModule.kt
+++ composeApp/src/androidMain/kotlin/com/garam/qook/di/PlatformModule.kt
@@ -1,8 +1,8 @@
 package com.garam.qook.di
 
 import com.garam.qook.com.garam.qook.data.getDatabaseBuilder
-import com.garam.qook.data.QookDatabase
-import com.garam.qook.data.getTodoDatabase
+import com.garam.qook.data.local.QookDatabase
+import com.garam.qook.data.local.getTodoDatabase
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
